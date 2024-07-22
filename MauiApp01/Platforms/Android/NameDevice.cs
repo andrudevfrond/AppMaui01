76c0f4c856747fd1a765847bb01153b2c20b60a7
Andres Galindo <sharlly1329g@gmail.com>
2024-07-22 18:49:19 -0500

    Se correge el metodo de obtener el nombre del dispositivo

--- MauiApp01/Platforms/Android/NameDevice.cs
+++ MauiApp01/Platforms/Android/NameDevice.cs
@@ -8,7 +8,7 @@ public partial class NameDevice
 {
     public partial string GetName()
     {
-        var location = Android.App.Application.Context.GetSystemService(Context.LocaleService).JavaCast<LocationManager>();
+        var location = Android.App.Application.Context.GetSystemService(Context.LocationService).JavaCast<LocationManager>();
         return location.GnssHardwareModelName;
     }
 }
