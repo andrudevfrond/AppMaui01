da1c2e95a373f6ace33e8f9456d6360d91cb005f
Andres Galindo <sharlly1329g@gmail.com>
2024-07-23 09:01:35 -0500

    Estados de Ciclo de vida de una app MAUI

--- MauiApp01/App.xaml.cs
+++ MauiApp01/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿namespace MauiApp01
+﻿
+namespace MauiApp01
 {
     public partial class App : Application
     {
@@ -6,11 +7,60 @@
         {
             InitializeComponent();
 
-            MainPage = new ContentPage() { 
-                Content = new Label() { 
-                    Text = new NameDevice().GetName(),
-                }
+            var button = new Button();
+            button.Text = "Generar un nueva ventana";
+            button.Clicked += (s, a) =>
+            {
+                var ventana2 = new Window(new ContentPage());
+                ventana2.Created += (s, a) => System.Diagnostics.Debug.WriteLine(nameof(ventana2.Created));
+                ventana2.Activated += (s, a) => System.Diagnostics.Debug.WriteLine(nameof(ventana2.Activated));
+                ventana2.Deactivated += (s, a) => System.Diagnostics.Debug.WriteLine(nameof(ventana2.Deactivated));
+                
+                OpenWindow(ventana2);
             };
+
+            MainPage = new ContentPage() { 
+                Content = button
+            };
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+        }
+
+        protected override void OnStart()
+        {
+            base.OnStart();
+        }
+
+        protected override void OnSleep()
+        {
+            base.OnSleep();
+        }
+
+        protected override Window CreateWindow(IActivationState? activationState)
+        {
+            var ventana = base.CreateWindow(activationState);
+            ventana.Created += Ventana_Created;
+            ventana.Deactivated += Ventana_Deactivated;
+            ventana.Activated += Ventana_Activated;
+            return ventana;
+        }
+
+        private void Ventana_Activated(object? sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(nameof(Ventana_Activated));
+        }
+
+        private void Ventana_Deactivated(object? sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(nameof(Ventana_Deactivated));
+        }
+
+        private void Ventana_Created(object? sender, EventArgs e)
+        {
+            System.Diagnostics.Debug.WriteLine(nameof(Ventana_Created));
         }
     }
 }
